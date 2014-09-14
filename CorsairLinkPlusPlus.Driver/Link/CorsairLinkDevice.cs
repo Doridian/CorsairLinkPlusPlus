f70ed2377bda2ade6aee023a7c3c4ec59f977815
Mark Dietzer <mriq91@gmail.com>
2014-09-15 01:38:25 +0200

    Add basic functionality

---
+++ CorsairLinkPlusPlus.Driver/Link/CorsairLinkDevice.cs
@@ -0,0 +1,89 @@
+﻿using CorsairLinkPlusPlus.Driver.USB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorsairLinkPlusPlus.Driver.Link
+{
+    public class CorsairLinkDevice
+    {
+        internal static CorsairLinkDevice CreateNew(CorsairLinkUSBDevice usbDevice, byte channel)
+        {
+            return new CorsairLinkDevice(usbDevice, channel);
+        }
+
+        private readonly CorsairLinkUSBDevice usbDevice;
+        private readonly byte channel;
+
+        internal CorsairLinkDevice(CorsairLinkUSBDevice usbDevice, byte channel)
+        {
+            this.usbDevice = usbDevice;
+            this.channel = channel;
+        }
+
+        protected byte ReadSingleByteRegister(byte register)
+        {
+            return usbDevice.SendCommand(0x07, channel, new byte[] { register })[0];
+        }
+
+        protected byte[] ReadRegister(byte register, byte bytes)
+        {
+            switch(bytes) {
+                case 1:
+                    return new byte[] { ReadSingleByteRegister(register) };
+                case 2:
+                    byte[] rawRet2 = usbDevice.SendCommand(0x09, channel, new byte[] { register });
+                    return new byte[] { rawRet2[0], rawRet2[1] };
+                default:
+                    byte[] rawRet = usbDevice.SendCommand(0x0B, channel, new byte[] { register, bytes });
+                    byte[] ret = new byte[rawRet[0]];
+                    Buffer.BlockCopy(rawRet, 1, ret, 0, ret.Length);
+                    return ret;
+            }
+        }
+
+        public int GetDeviceID()
+        {
+            return ReadSingleByteRegister(0x00);
+        }
+
+        public int GetFirmwareVersion()
+        {
+            return BitConverter.ToInt16(ReadRegister(0x01, 2), 0);
+        }
+
+        public static string ByteArrayToHexString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+        public string GetDeviceName()
+        {
+            byte[] reg = ReadRegister(0x02, 8);
+            string ret = System.Text.Encoding.ASCII.GetString(reg);
+            if (string.IsNullOrEmpty(ret))
+                return "N/A";
+            return ByteArrayToHexString(reg);
+        }
+
+        public int GetFanCount()
+        {
+            return ReadSingleByteRegister(0x11);
+        }
+
+        public int GetTemperatureCount()
+        {
+            return ReadSingleByteRegister(0x0D);
+        }
+
+        public int GetLEDCount()
+        {
+            return ReadSingleByteRegister(0x05);
+        }
+    }
+}
---
+++ CorsairLinkPlusPlus.Driver/USB/CorsairLinkUSBDevice.cs
@@ -0,0 +1,122 @@
+﻿using CorsairLinkPlusPlus.Driver.Link;
+using HidLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorsairLinkPlusPlus.Driver.USB
+{
+    public class CorsairLinkUSBDevice
+    {
+        private readonly HidDevice hidDevice;
+        protected int commandNo = 20;
+
+        internal CorsairLinkUSBDevice(HidDevice hidDevice)
+        {
+            this.hidDevice = hidDevice;
+        }
+
+        protected virtual byte[] MakeCommand(byte opcode, byte channel, byte[] payload)
+        {
+            byte[] _command = new byte[65];
+            _command[0] = 0;
+            _command[2] = (byte)(commandNo++);
+            if (commandNo > 255)
+                commandNo = 20;
+            _command[3] = (byte)(opcode | (channel << 4));
+            if (payload != null)
+            {
+                _command[1] = (byte)(payload.Length + 2);
+                Buffer.BlockCopy(payload, 0, _command, 4, payload.Length);
+            }
+            else
+            {
+                _command[1] = 2;
+            }
+            return _command;
+        }
+
+        public List<byte> GetUsedChannels()
+        {
+            byte[] cmdRes = SendCommand(0x4F, 0x00, null);
+            List<byte> usedChannels = new List<byte>();
+            for (byte i = 0; i < 8; i++)
+            {
+                if (cmdRes[i] != 0xFF)
+                {
+                    usedChannels.Add(i);
+                }
+            }
+            return usedChannels;
+        }
+
+        public List<CorsairLinkDevice> GetSubDevices()
+        {
+            List<CorsairLinkDevice> ret = new List<CorsairLinkDevice>();
+
+            foreach(byte channel in GetUsedChannels())
+            {
+                ret.Add(GetDeviceOnChannel(channel));
+            }
+
+            return ret;
+        }
+
+        public CorsairLinkDevice GetDeviceOnChannel(byte channel)
+        {
+            return CorsairLinkDevice.CreateNew(this, channel);
+        }
+
+        protected virtual byte[] ParseResponse(byte[] response)
+        {
+            byte[] _response = new byte[60];
+            Buffer.BlockCopy(response, 3, _response, 0, _response.Length);
+            return _response;
+        }
+
+        protected byte[] ReadResponse()
+        {
+            HidDeviceData response = hidDevice.Read(500);
+            return ParseResponse(response.Data);
+        }
+
+        public byte[] SendCommand(byte opcode, byte channel, byte[] command)
+        {
+            hidDevice.Write(MakeCommand(opcode, channel, command), 500);
+            return ReadResponse();
+        }
+    }
+
+    public class CorsairLinkUSBDeviceOld : CorsairLinkUSBDevice
+    {
+        internal CorsairLinkUSBDeviceOld(HidDevice hidDevice)
+            : base(hidDevice)
+        {
+
+        }
+
+        protected override byte[] MakeCommand(byte opcode, byte channel, byte[] payload)
+        {
+            byte[] _command = new byte[65];
+            _command[0] = 0;
+            _command[1] = (byte)(commandNo++);
+            if (commandNo > 255)
+                commandNo = 20;
+            _command[2] = (byte)(opcode | (channel << 4));
+            if (payload != null)
+            {
+                Buffer.BlockCopy(payload, 0, _command, 3, payload.Length);
+            }
+            return _command;
+        }
+
+        protected override byte[] ParseResponse(byte[] response)
+        {
+            byte[] _response = new byte[60];
+            Buffer.BlockCopy(response, 2, _response, 0, _response.Length);
+            return _response;
+        }
+    }
+}
