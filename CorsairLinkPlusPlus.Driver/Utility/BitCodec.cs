348cb3a71f8387936773436622797b1ee8ac5cc4
Mark Dietzer <mriq91@gmail.com>
2014-09-19 12:09:42 +0200

    Improve float functions a bit

--- CorsairLinkPlusPlus.Driver/Utility/BitCodec.cs
+++ CorsairLinkPlusPlus.Driver/Utility/BitCodec.cs
@@ -7,25 +7,29 @@ namespace CorsairLinkPlusPlus.Driver
     {
         public static double ToFloat(ushort input_val)
         {
-            int num = input_val >> 11;
-            int num2 = (int)(input_val & 2047);
-            if (num > 15)
-                num = -(32 - num);
+            int _exponent = input_val >> 11;
+            int _fraction = (int)(input_val & 2047);
+            if (_exponent > 15)
+                _exponent = -(32 - _exponent);
 
-            if (num2 > 1023)
-                num2 = -(2048 - num2);
+            if (_fraction > 1023)
+                _fraction = -(2048 - _fraction);
 
-            if ((num2 & 1) == 1)
-                num2++;
-            double num3 = (double)num2 * Math.Pow(2.0, (double)num);
-            return (double)((int)(num3 * 10.0 + 0.5)) / 10.0;
+            if ((_fraction & 1) == 1)
+                _fraction++;
+
+            double _rawNumber = (double)_fraction * Math.Pow(2.0, (double)_exponent);
+
+            return (double)((int)(_rawNumber * 10.0 + 0.5)) / 10.0;
         }
+
         public static double ToFloat(byte[] data, int start_index = 0)
         {
             if (data == null)
                 return 0.0;
             return ToFloat(BitConverter.ToUInt16(data, start_index));
         }
+
         public static byte[] FromFloat(double value, int exponent)
         {
             byte[] array = new byte[2];
