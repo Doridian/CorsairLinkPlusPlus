d6e6bc62474d44a43bf6703589db8c219d01b7b9
Mark Dietzer <mriq91@gmail.com>
2014-09-30 19:14:08 +0200

    Refactor refreshes a bit

--- CorsairLinkPlusPlus.CLI/Program.cs
+++ CorsairLinkPlusPlus.CLI/Program.cs
@@ -20,15 +20,65 @@
 
 using CorsairLinkPlusPlus.Common;
 using CorsairLinkPlusPlus.Common.Controller;
+using CorsairLinkPlusPlus.Common.Registry;
 using CorsairLinkPlusPlus.Common.Sensor;
 using CorsairLinkPlusPlus.Common.Utility;
+using Microsoft.Win32;
 using System;
 using System.Threading;
+using System.Linq;
 
 namespace CorsairLinkPlusPlus.CLI
 {
     class Program
     {
+        private static void FixUpRegistry()
+        {
+            bool flag = false;
+            try
+            {
+                RegistryKey registryKey = Registry.LocalMachine.OpenSubKey("SYSTEM\\ControlSet001\\Enum\\USB\\VID_1B1C&PID_0C04");
+                string[] subKeyNames = registryKey.GetSubKeyNames();
+                for (int i = 0; i < subKeyNames.Length; i++)
+                {
+                    string str = subKeyNames[i];
+                    RegistryKey registryKey2 = registryKey.OpenSubKey(str + "\\Device Parameters", true);
+                    flag |= ((int)registryKey2.GetValue("DeviceSelectiveSuspended", 0) != 0);
+                    flag |= ((int)registryKey2.GetValue("EnhancedPowerManagementEnabled", 0) != 0);
+                    flag |= ((int)registryKey2.GetValue("SelectiveSuspendEnabled", 0) != 0);
+                    registryKey2.SetValue("DeviceSelectiveSuspended", 0);
+                    registryKey2.SetValue("SelectiveSuspendEnabled", 0);
+                    registryKey2.SetValue("EnhancedPowerManagementEnabled", 0);
+                    if (registryKey2.GetSubKeyNames().Contains("AllowIdleIrpInD3"))
+                    {
+                        if (registryKey2.GetValueKind("AllowIdleIrpInD3") == RegistryValueKind.DWord)
+                        {
+                            flag |= ((int)registryKey2.GetValue("AllowIdleIrpInD3", 0) != 0);
+                            registryKey2.SetValue("AllowIdleIrpInD3", 0);
+                        }
+                        else
+                        {
+                            if (registryKey2.GetValueKind("AllowIdleIrpInD3") == RegistryValueKind.Binary)
+                            {
+                                bool arg_176_0 = flag;
+                                RegistryKey arg_164_0 = registryKey2;
+                                string arg_164_1 = "AllowIdleIrpInD3";
+                                byte[] array = new byte[1];
+                                flag = (arg_176_0 | ((byte[])arg_164_0.GetValue(arg_164_1, array))[0] != 0);
+                                RegistryKey arg_188_0 = registryKey2;
+                                string arg_188_1 = "AllowIdleIrpInD3";
+                                array = new byte[1];
+                                arg_188_0.SetValue(arg_188_1, array);
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+
         static void PrintSensorsAndSubDevices(IDevice device, string prefix)
         {
             if (!device.Valid || !device.Present)
@@ -42,6 +92,12 @@ namespace CorsairLinkPlusPlus.CLI
                 {
                     ILED led = (ILED)device;
                     IControllableSensor controllableLED = (IControllableSensor)device;
+
+                    {
+                        IFixedColorController _ledController = (IFixedColorController)ControllerRegistry.Get("LED.CorsairLink.SingleColor").New();
+                        _ledController.Value = new Color(255, 1, 1);
+                        controllableLED.Controller = _ledController;
+                    }
 
                     IController ledController = controllableLED.Controller;
                     Console.Out.WriteLine(prefix + "- " + led.Name + " = " + led.Value + " " + led.Unit.GetPostfix());
@@ -92,6 +148,8 @@ namespace CorsairLinkPlusPlus.CLI
 
         static void Main(string[] args)
         {
+            FixUpRegistry();
+
             while (true)
             {
                 Console.Clear();
