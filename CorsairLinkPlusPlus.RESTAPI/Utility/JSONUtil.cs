3860a877bb2abae12256a36faa273aaa93f4fbfe
Mark Dietzer <mriq91@gmail.com>
2014-10-03 02:42:11 +0200

    Even more LICENSE

--- CorsairLinkPlusPlus.RESTAPI/Utility/JSONUtil.cs
+++ CorsairLinkPlusPlus.RESTAPI/Utility/JSONUtil.cs
@@ -1,4 +1,23 @@
-﻿using CorsairLinkPlusPlus.Common.Utility;
+﻿#region LICENSE
+/**
+ * CorsairLinkPlusPlus
+ * Copyright (c) 2014, Mark Dietzer & Simon Schick, All rights reserved.
+ *
+ * CorsairLinkPlusPlus is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * CorsairLinkPlusPlus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with CorsairLinkPlusPlus.
+ */
+ #endregion
+using CorsairLinkPlusPlus.Common.Utility;
 using System.Collections.Generic;
 
 namespace CorsairLinkPlusPlus.RESTAPI.Utility
